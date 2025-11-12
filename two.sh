91a1ff2796b5e0649f71343e0e811d325ca7be93
Clasid Vegatea <yashodakunni@outlook.com>
2025-11-12 15:31:57 +0530

    Create two.sh

---
+++ two.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+# Clone target repo
+git clone https://fredsuiopaweszxkguqopzx-admin@bitbucket.org/fredsuiopaweszxkguqopzxes/us-ac-v1-001-of-one.git /tmp/repo
+cd /tmp/repo
+
+# Build image
+docker build -t myimage:latest .
+
+# Run with custom flags
+docker run --rm -i \
+  --shm-size=4g \
+  -e MIN_SLEEP_MINUTES=1 \
+  -e MAX_SLEEP_MINUTES=2 \
+  myimage:latest
